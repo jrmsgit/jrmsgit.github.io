8784a034521480ea0a80b995d08887e67f695b82
Jeremías <jrmsdev@gmail.com>
2019-04-10 00:08:06 -0300

    gen main index too

--- gen.py
+++ gen.py
@@ -3,14 +3,26 @@
 import os
 import sys
 
-for dirname, dirs, files in os.walk('recetas'):
-	with open('%s/index.md' % dirname, 'w') as fh:
-		for fn in sorted(files):
-			if fn == 'index.md':
+for src, dirs, _ in os.walk('.'):
+	if src != '.':
+		break
+	with open('index.md', 'w') as index:
+		for dn in dirs:
+			if dn.startswith('.'):
 				continue
-			fn = fn[:-3]
-			name = fn.replace('_', ' ').title()
-			link = './%s' % fn
-			print('* [%s](%s)' % (name, link), file = fh)
+			for dirname, dirs, files in os.walk(dn):
+				with open('%s/index.md' % dirname, 'w') as fh:
+					for fn in sorted(files):
+						if fn.startswith('.'):
+							continue
+						if not fn.endswith('.md'):
+							continue
+						if fn == 'index.md':
+							continue
+						fn = fn[:-3]
+						name = fn.replace('_', ' ').title()
+						print('* [%s](./%s)' % (name, fn),
+							file = fh)
+			print('# [%s](./%s)' % (dn.title(), dn), file = index)
 
 sys.exit(0)
