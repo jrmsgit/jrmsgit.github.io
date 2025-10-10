e85cea0438d5af3f0df0f1725fb2bb626c1694cf
Jeremías <jrmsdev@gmail.com>
2025-10-10 00:25:40 -0300

    hugo install machine arch

--- hugo/install.sh
+++ hugo/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -eu
 HUGO_VERSION=0.150.0
+ARCH=$(dpkg --print-architecture)
 wget -q -O /tmp/hugo.deb \
-	https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
+	"https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-${ARCH}.deb"
 dpkg -i /tmp/hugo.deb
 rm -f /tmp/hugo.deb
 exit 0
